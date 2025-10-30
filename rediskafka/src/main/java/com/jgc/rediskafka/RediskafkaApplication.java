14ee0dafa10942a041cb9224b19351f0d1a99423
Joseph Cramsey <josephcsoftware@gmail.com>
2025-10-30 13:52:40 -0400

    First commit

---
+++ rediskafka/src/main/java/com/jgc/rediskafka/RediskafkaApplication.java
@@ -0,0 +1,13 @@
+package com.jgc.rediskafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RediskafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RediskafkaApplication.class, args);
+	}
+
+}
---
+++ rediskafka/src/test/java/com/jgc/rediskafka/RediskafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jgc.rediskafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RediskafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
